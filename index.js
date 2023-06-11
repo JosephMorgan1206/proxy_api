8a54ce3ff4735a6baf599a8821f9fa2e8e83b5ae
josephmorgan <josephmorgan1206@gmail.com>
2023-06-11 16:18:59 +1000

    second commit

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 
+const { default: axios } = require("axios");
 const express = require("express");
 require('dotenv').config()
 
 
-const port = 3000;
+const port = 5000;
 
 
 const app = express();
 const https = require('https')
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || port, () => {
 	console.log(`App start on port ${port}`);
 });
 
 app.get('/api/v1/klines', async function (req, res) {
+	console.log('calling here');
+	const result = await axios.get('https://pro.apex.exchange/api/v1/klines?end=1686437880&interval=30&start=1685861940&symbol=BTCUSDC');
+	console.log('debug', result.data)
+	res.set({ 'Content-Type': 'text/event-stream', 'access-control-allow-origin': '*' })
+	res.status(200).send(result.data)
 
-	var options = {
-		hostname: 'pro.apex.exchange',
-		path: '/api/v1/klines?start=' + req.query.start + '&end=' + req.query.end + '&interval=' + req.query.inerval + '&symbol=' + req.query.symbol,
-	};
+	// var options = {
+	// 	hostname: 'pro.apex.exchange',
+	// 	path: '/api/v1/klines?start=' + req.query.start + '&end=' + req.query.end + '&interval=' + req.query.inerval + '&symbol=' + req.query.symbol,
+	// };
 
-	callback = function (response) {
-		var str = '';
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
+	// callback = function (response) {
+	// 	var str = '';
+	// 	response.on('data', function (chunk) {
+	// 		str += chunk;
+	// 	});
 
-		response.on('end', function () {
-			res.set({ 'Content-Type': 'text/event-stream', 'access-control-allow-origin': '*' })
-			res.send(JSON.parse(str))
-		});
-	}
+	// 	response.on('end', function () {
+	// 		res.set({ 'Content-Type': 'text/event-stream', 'access-control-allow-origin': '*' })
+	// 		res.send(JSON.parse(str))
+	// 	});
+	// }
 
-	https.request(options, callback).end();
+	// https.request(options, callback).end();
 })
