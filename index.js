bb1bb1e2f7b0c6f6dfe1755c850e908b622fcd29
josephmorgan <josephmorgan1206@gmail.com>
2023-06-11 16:50:59 +1000

    fourth commit

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ app.listen(process.env.PORT || port, () => {
 
 app.get('/api/v1/klines', async function (req, res) {
 	const result = await axios.get(`https://pro.apex.exchange/api/v1/klines?end=${req.query.end}&interval=${req.query.interval}&start=${req.query.start}&symbol=${req.query.symbol}`);
-	console.log('debug', result.data)
 	res.set({ 'Content-Type': 'text/event-stream', 'access-control-allow-origin': '*' })
-	res.status(200).send(result.data)
+	res.status(200).send(result.data.data[req.query.symbol])
 
 	// var options = {
 	// 	hostname: 'pro.apex.exchange',
